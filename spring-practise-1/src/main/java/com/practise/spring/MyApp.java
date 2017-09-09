c2ab3106c25fe965976229363dcef172f4dd49e9
Pratik <pratikaambani@gmail.com>
2017-09-09 11:10:02 +0530

    added 001 base package

--- spring-practise-1/src/main/java/com/practise/spring/MyApp.java
+++ spring-practise-1/src/main/java/com/practise/spring/MyApp.java
@@ -1,15 +1,29 @@
 package com.practise.spring;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Created by Pratik Ambani on 9/09/2017.
  */
 public class MyApp {
     public static void main(String[] args) {
 
+        String applicationContextPath = "applicationContext.xml";
+
         Coach coach1 = new BaseballCoach();
         System.out.println(coach1.getDailyWorkout());
 
         Coach coach2 = new CricketCoach();
         System.out.println(coach2.getDailyWorkout());
+
+
+/*
+        ApplicationContext context = new ClassPathXmlApplicationContext(applicationContextPath);
+
+        Coach myCoach = (Coach) context.getBean("myCoachBean", Coach.class);
+        System.out.println(myCoach.getDailyWorkout());
+*/
+
     }
 }
