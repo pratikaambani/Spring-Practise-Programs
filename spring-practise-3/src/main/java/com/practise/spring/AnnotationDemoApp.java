ab93fca2de5ad048c3b8d746c4da213c3a337dce
Pratik <pratikaambani@gmail.com>
2017-09-09 17:51:12 +0530

    added 011 using default bean value

--- spring-practise-3/src/main/java/com/practise/spring/AnnotationDemoApp.java
+++ spring-practise-3/src/main/java/com/practise/spring/AnnotationDemoApp.java
@@ -10,7 +10,8 @@ public class AnnotationDemoApp {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        Coach coach = context.getBean("thatSillyCoach", TennisCoach.class);
+        //TODO 011: Default bean id by removing custom bean ID in TennisCoach class
+        Coach coach = context.getBean("tennisCoach", Coach.class);
         System.out.println(coach.getDailyWorkout());
     }
 }
--- spring-practise-3/src/main/java/com/practise/spring/TennisCoach.java
+++ spring-practise-3/src/main/java/com/practise/spring/TennisCoach.java
@@ -5,7 +5,8 @@ import org.springframework.stereotype.Component;
 /**
  * Created by Pratik Ambani on 9/09/2017.
  */
-@Component("thatSillyCoach")
+//TODO 011: Default bean id by removing custom bean ID in TennisCoach class
+@Component
 public class TennisCoach implements Coach {
 
     @Override
