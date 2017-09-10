71ff35444f294f86d0131591e7be76cb0ed0499e
Pratik <pratikaambani@gmail.com>
2017-09-10 16:37:37 +0530

    added 030 retrieve dropdown values from Java Class

--- spring-practise-7/src/main/java/com/practise/spring/Student.java
+++ spring-practise-7/src/main/java/com/practise/spring/Student.java
@@ -1,5 +1,7 @@
 package com.practise.spring;
 
+import java.util.LinkedHashMap;
+
 /**
  * Created by Pratik Ambani on 10/09/2017.
  */
@@ -9,6 +11,12 @@ public class Student {
     private String lName;
 
     public Student() {
+
+        //TODO 030 We can also retrieve dropdown values from Java Class
+        countryOptions = new LinkedHashMap();
+        countryOptions.put("IN", "India");
+        countryOptions.put("UK", "United Kingdom");
+        countryOptions.put("US", "Unites States");
     }
 
     public String getfName() {
@@ -36,5 +44,12 @@ public class Student {
 
     public void setCountry(String country) {
         this.country = country;
+    }
+
+    //TODO 030 We can also retrieve dropdown values from Java Class
+    private LinkedHashMap<String, String> countryOptions;
+
+    public LinkedHashMap<String, String> getCountryOptions() {
+        return countryOptions;
     }
 }
--- spring-practise-7/src/main/webapp/WEB-INF/view/student-form.jsp
+++ spring-practise-7/src/main/webapp/WEB-INF/view/student-form.jsp
@@ -11,9 +11,13 @@
     LastName: <form:input path="lName" /> <br />
     <%--TODO 029 Dropdown options--%>
     <form:select path="country">
+<%--TODO 030 We can also retrieve dropdown values from Java Class--%>
+<%-- Uncomment this to run 029
         <form:option value="IN" label="IN" />
         <form:option value="US" label="US" />
         <form:option value="UK" label="UK" />
+--%>
+        <form:options items="${studentAttr.countryOptions}" />
     </form:select>
     <input type="submit" value="Submit">
 </form:form>
