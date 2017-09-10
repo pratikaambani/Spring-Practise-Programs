f5952b46b78a5fcba33ddec75c80397a308fd354
Pratik <pratikaambani@gmail.com>
2017-09-10 07:02:07 +0530

    updated 020 @Bean annotations

--- spring-practise-4/src/main/java/com/practise/spring/bean/SportsConfig.java
+++ spring-practise-4/src/main/java/com/practise/spring/bean/SportsConfig.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Configuration;
  * Created by Pratik Ambani on 9/09/2017.
  */
 @Configuration
-//@ComponentScan("com.practise.spring")
+@ComponentScan("com.practise.spring")
 public class SportsConfig {
 
     //TODO 020: Defining beans
