c8cd29319285303c52249e97a4ac33e8bfec0860
Pratik <pratikaambani@gmail.com>
2017-09-16 23:31:02 +0530

    added 040 JoinPoint and MethodSignature to read values

--- spring-practise-11/src/main/java/com/practise/aspect/MyLoggingAspect.java
+++ spring-practise-11/src/main/java/com/practise/aspect/MyLoggingAspect.java
@@ -1,7 +1,11 @@
 package com.practise.aspect;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.*;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
 
 /**
  * Created by Pratik Ambani on 16/09/2017.
@@ -16,8 +20,17 @@ public class MyLoggingAspect {
     }
 
     @Before("declaringPointcutForDaoPkg()")
-    public void beforeAnyMethodInDao() {
+    public void beforeAnyMethodInDao(JoinPoint joinPoint) {
         System.out.println("Before any method");
+        //TODO 040: display method signature and arguments
+        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+        System.out.println("Method signature: " +methodSignature);
+
+        Object[] arguments = joinPoint.getArgs();
+
+        for(Object tempArg : arguments) {
+            System.out.println("This is printed from Aspect, tempArg: " +tempArg);
+        }
     }
 
 }
